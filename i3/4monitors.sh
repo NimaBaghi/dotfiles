75ceb8a439fdb95a7d4acc01c3e7b516d0683101
Nima Baghi <nimaba1999@gmail.com>
2022-06-14 14:34:34 +0430

    remove useless configs for kde

--- i3/4monitors.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-1.1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 4480x360 --rotate normal --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --mode 1920x1080 --pos 0x360 --rotate normal
